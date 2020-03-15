52b13a39457a341ec561453ebaaa10a37adff14a
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-03-15 11:47:38 -0400

    Create deleteDuplicates(ListNode head).cpp

---
+++ deleteDuplicates(ListNode head).cpp
@@ -0,0 +1,42 @@
+/*
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+        if (!head) return head;
+        
+        ListNode* p1 = nullptr;
+        ListNode* p2 = head;
+        
+        while(p2){
+            if(!p1){
+                p1 = p2;
+                p2 = p2->next;
+                continue;
+            }
+            
+            if(p2->val == p1->val){
+                p2 = p2->next;
+                continue;
+            }
+            p1->next = p2;
+            p1 = p2;
+            p2 = p2->next;
+            
+        }
+    
+        p1->next = nullptr;
+        return head;
+    }
+};
