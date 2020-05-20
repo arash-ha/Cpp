62e7816e1847d1d590d515b3f13e9f08c96e9574
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-05-20 09:12:09 -0400

    Create pathSum(TreeNode root, int sum)2.cpp

---
+++ pathSum(TreeNode root, int sum)2.cpp
@@ -0,0 +1,54 @@
+/*
+Path Sum III
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int c = 0;
+    int pathSum(TreeNode* root, int sum) {
+        if(!root) return 0;
+        return helper(root, sum) + pathSum(root -> left, sum) + pathSum(root -> right, sum);
+    }
+    
+private:
+    int helper(TreeNode* root, int sum){
+        if(!root) return 0;
+        return (sum == root -> val) + helper(root -> left, sum - root -> val) + helper(root -> right, sum - root -> val);
+    }
+};
+
+
