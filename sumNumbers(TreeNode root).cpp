c24d98c98d28b12ad4c22e3bb43882b15a9b0b30
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-05-27 09:23:38 -0400

    Create sumNumbers(TreeNode root).cpp

---
+++ sumNumbers(TreeNode root).cpp
@@ -0,0 +1,68 @@
+/*
+Sum Root to Leaf Numbers
+Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+
+Find the total sum of all root-to-leaf numbers.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Input: [1,2,3]
+    1
+   / \
+  2   3
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+
+Input: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        string s{""};
+        int n{0}, sum{0};
+        helper(root, s, n, sum);
+        return sum;
+    }
+    void helper(TreeNode* root, string s, int n, int& sum){
+        if(!root) return;
+        s += to_string(root -> val);
+        if(!root -> left && !root -> right){
+            n = std::stoi(s);
+            sum += n;
+        }
+        helper(root -> left, s, n, sum);
+        helper(root -> right, s, n, sum);
+    }
+};
