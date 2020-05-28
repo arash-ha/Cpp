ff881dda8adca1e2bbe09892f7c30e37006bc881
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-05-28 14:38:37 -0400

    Create isCompleteTree(TreeNode root).cpp

---
+++ isCompleteTree(TreeNode root).cpp
@@ -0,0 +1,66 @@
+/*
+Check Completeness of a Binary Tree
+Given a binary tree, determine if it is a complete binary tree.
+
+Definition of a complete binary tree from Wikipedia:
+In a complete binary tree every level, except possibly the last, is completely filled, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+ 
+
+Example 1:
+
+
+
+Input: [1,2,3,4,5,6]
+Output: true
+Explanation: Every level before the last is full (ie. levels with node-values {1} and {2, 3}), and all nodes in the last level ({4, 5, 6}) are as far left as possible.
+Example 2:
+
+
+
+Input: [1,2,3,4,5,null,7]
+Output: false
+Explanation: The node with value 7 isn't as far left as possible.
+ 
+Note:
+
+The tree will have between 1 and 100 nodes.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        queue<TreeNode*> q;
+        bool flag{false};
+        
+        if(!root) return true;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* cur = q.front();
+            q.pop();
+            if(cur -> left){
+                if(flag == true) return false;
+                q.push(cur -> left);
+            }
+            else flag = true;
+            
+            if(cur -> right){
+                if(flag == true) return false;
+                q.push(cur -> right);
+            }
+            else flag = true;
+        }
+        return true;
+    }
+};
