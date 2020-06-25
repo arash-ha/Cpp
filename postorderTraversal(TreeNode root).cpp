40030b32818b611631e9af16a0ab6b8029f7cfb8
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-06-25 08:11:57 -0400

    Create postorderTraversal(TreeNode root).cpp

---
+++ postorderTraversal(TreeNode root).cpp
@@ -0,0 +1,40 @@
+/*
+Binary Tree Postorder Traversal
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [3,2,1]
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> v;
+        helper(root, v);
+        return v;
+    }
+    void helper(TreeNode* root, vector<int>& v){
+        if(!root) return;
+        helper(root -> left, v);
+        helper(root -> right, v);
+        v.emplace_back(root -> val);
+    }
+};
