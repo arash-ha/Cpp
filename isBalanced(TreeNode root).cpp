2d99345b488d9dcb57e06e378cb469c762815d8d
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-07-02 07:35:16 -0400

    Create isBalanced(TreeNode root).cpp

---
+++ isBalanced(TreeNode root).cpp
@@ -0,0 +1,58 @@
+/*
+Balanced Binary Tree
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+ 
+
+Example 1:
+
+Given the following tree [3,9,20,null,null,15,7]:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+Return true.
+
+Example 2:
+
+Given the following tree [1,2,2,3,3,null,null,4,4]:
+
+       1
+      / \
+     2   2
+    / \
+   3   3
+  / \
+ 4   4
+Return false.
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return helper(root) != -1;
+    }
+    int helper(TreeNode* root){
+        if(!root) return 0;
+        int lh = helper(root -> left);
+        int rh = helper(root -> right);
+        if (lh == -1 || rh == -1 || abs(lh - rh) > 1) return -1;
+        return max(lh, rh) + 1;
+    }
+};
