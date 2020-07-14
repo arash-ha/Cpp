a133afc1ebe650848d586c6409b62078679d249f
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-07-14 15:41:32 -0400

    Create toLowerCase(string str).cpp

---
+++ toLowerCase(string str).cpp
@@ -0,0 +1,30 @@
+/*
+To Lower Case
+Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+ 
+
+Example 1:
+
+Input: "Hello"
+Output: "hello"
+Example 2:
+
+Input: "here"
+Output: "here"
+Example 3:
+
+Input: "LOVELY"
+Output: "lovely"
+*/
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for(char &c: str)
+            if(isupper(c)) c = tolower(c);
+        
+        return str;
+    }
+};
+
