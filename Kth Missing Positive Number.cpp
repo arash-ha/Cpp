63f8a8d5e582525f55e56434f1ebb326aacc0652
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-10-25 13:34:25 -0400

    Create Kth Missing Positive Number.cpp

---
+++ Kth Missing Positive Number.cpp
@@ -0,0 +1,54 @@
+/*
+Kth Missing Positive Number
+
+Given an array arr of positive integers sorted in a strictly increasing order, and an integer k.
+Find the kth positive integer that is missing from this array.
+
+Example 1:
+
+Input: arr = [2,3,4,7,11], k = 5
+Output: 9
+Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The 5th missing positive integer is 9.
+Example 2:
+
+Input: arr = [1,2,3,4], k = 2
+Output: 6
+Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing positive integer is 6.
+ 
+
+Constraints:
+
+1 <= arr.length <= 1000
+1 <= arr[i] <= 1000
+1 <= k <= 1000
+arr[i] < arr[j] for
+
+*/
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) 
+    {
+        unordered_map<int,int> ump;
+        for(auto i : arr) ump[i]++;
+        
+        vector<pair<int, int>> vec;
+        for(int i = 1; i <= 10000; i++){
+            if(ump.find(i) == ump.end())
+                vec.emplace_back(make_pair(i,0));
+            else
+                vec.emplace_back(make_pair(i,1));
+        }
+        
+        for(int i = 0; i <= 10000; i++){
+            if(vec[i].second == 0) k--;
+            
+            if(k==0){
+                return vec[i].first;
+                break;
+            }
+        }
+        
+        return 0;
+        
+    }
+};
