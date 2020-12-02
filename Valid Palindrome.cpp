c571b73cd99ead99f494c933ee34ab4ee0ff247a
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-12-02 12:28:32 -0500

    Create Valid Palindrome.cpp

---
+++ Valid Palindrome.cpp
@@ -0,0 +1,30 @@
+/*
+Valid Palindrome
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+*/
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j){
+            while(i < j && !isalnum(s[i])) i++;
+            while(i < j && !isalnum(s[j])) j--;
+            if(tolower(s[i]) != tolower(s[j])) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
