daa2cf724adbfa6936e2dff34e980152832856ef
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-12-16 14:11:20 -0500

    Update Validate Binary Search Tree.cpp

--- Validate Binary Search Tree.cpp
+++ Validate Binary Search Tree.cpp
@@ -28,6 +28,11 @@ Example 2:
 Input: [5,1,4,null,null,3,6]
 Output: false
 Explanation: The root node's value is 5 but its right child's value is 4.
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-2^31 <= Node.val <= 2^31 - 1
 */
 
 /**
