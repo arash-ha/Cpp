57cfb26aeeb4d24d0287b451e2e849c07e17d189
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2020-12-29 18:04:11 -0500

    Update Swap Nodes in Pairs.cpp

--- Swap Nodes in Pairs.cpp
+++ Swap Nodes in Pairs.cpp
@@ -20,6 +20,7 @@ Given 1->2->3->4, you should return the list as 2->1->4->3.
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ // Solution 1
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
@@ -29,5 +30,26 @@ public:
         ptr1 -> next = head;
         head -> next = swapPairs(ptr2);
         return ptr1;       
+    }
+};
+
+// Solution 2
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head || !head->next)
+            return head;
+        ListNode *ptr1 = head, *ptr2;
+        head = head->next;
+        while(ptr1 && ptr1->next){
+            ptr2 = ptr1->next->next;
+            ptr1->next->next = ptr1;
+            if(!ptr2 || !(ptr2->next))
+                ptr1->next = ptr2;
+            else
+                ptr1->next = ptr2->next;
+            ptr1 = ptr2;
+        }
+        return head;
     }
 };
