a42a9415d29b1ac874a0e1d53f8fe88f7f8979ad
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-01-24 18:48:27 -0500

    Create Merge k Sorted Lists.cpp

---
+++ Merge k Sorted Lists.cpp
@@ -0,0 +1,82 @@
+/*
+Merge k Sorted Lists
+
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+Example 1:
+
+Input: lists = [[1,4,5],[1,3,4],[2,6]]
+Output: [1,1,2,3,4,4,5,6]
+Explanation: The linked-lists are:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+merging them into one sorted list:
+1->1->2->3->4->4->5->6
+Example 2:
+
+Input: lists = []
+Output: []
+Example 3:
+
+Input: lists = [[]]
+Output: []
+ 
+
+Constraints:
+
+k == lists.length
+0 <= k <= 10^4
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] is sorted in ascending order.
+The sum of lists[i].length won't exceed 10^4.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size() == 0)
+            return nullptr;
+        ListNode* resHead = lists[0];
+        for(int i = 1; i < lists.size(); i++)
+            resHead = mergeTwoLists(resHead, lists[i]);
+        return resHead;
+    }
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2){
+        ListNode* dummy = new ListNode(-1);
+        ListNode* cur = dummy;
+        while(l1 && l2){
+            if(l1->val <= l2->val){
+                cur->next = l1;
+                l1 = l1->next;
+            }
+            else{
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+        if(l1)
+            cur->next = l1;
+        if(l2)
+            cur->next = l2;
+        return dummy->next;
+    }
+};
