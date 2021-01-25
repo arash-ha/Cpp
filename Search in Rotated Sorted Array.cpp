378a0252343cae10e89f72b94be701d2e533d0fe
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-01-25 11:26:54 -0500

    Create Search in Rotated Sorted Array.cpp

---
+++ Search in Rotated Sorted Array.cpp
@@ -0,0 +1,63 @@
+/*
+Search in Rotated Sorted Array
+
+You are given an integer array nums sorted in ascending order (with distinct values), and an integer target.
+
+Suppose that nums is rotated at some pivot unknown to you beforehand (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+
+If target is found in the array return its index, otherwise, return -1.
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is guaranteed to be rotated at some pivot.
+-104 <= target <= 104
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[mid] > nums[high]){
+                if(target < nums[mid] && target >= nums[low])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            else if(nums[mid] < nums[low]){
+                if(target > nums[mid] && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+            else{
+                if(target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
