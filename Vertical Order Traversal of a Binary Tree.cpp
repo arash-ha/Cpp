ba5c4c5e544c88e0b809dddf195d4ec85138b68c
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-01-29 11:30:06 -0500

    Update Vertical Order Traversal of a Binary Tree.cpp

--- Vertical Order Traversal of a Binary Tree.cpp
+++ Vertical Order Traversal of a Binary Tree.cpp
@@ -45,6 +45,56 @@ However, in the report "[1,5,6]", the node value of 5 comes first since 5 is sma
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ 
+// Solution 1
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        vector<vector<int>> res;
+        if(!root)
+            return res;
+        unordered_map<int, vector<pair<int, int>>> cache;
+        int minC = 0, maxC = 0;
+        dfs(root, 0, 0, cache, minC, maxC);
+        for(int c = minC; c <= maxC; c++){
+            sort(cache[c].begin(), cache[c].end(), [](pair<int, int>& p1, pair<int, int>& p2){
+                return (p1.first < p2.first) || ((p1.first == p2.first) && (p1.second < p2.second));
+            });
+            vector<int> col;
+            for(pair<int, int>& p : cache[c])
+                col.push_back(p.second);
+            res.push_back(col);
+        }
+        return res;
+    }
+    void dfs(TreeNode* node, int r, int c, unordered_map<int, vector<pair<int, int>>>& cache, int& minC, int& maxC){
+        if(!node)
+            return;
+        if(cache.count(c))
+            cache[c].push_back({r, node->val});
+        else
+            cache.insert({c, {{r, node->val}}});
+        minC = min(minC, c);
+        maxC = max(maxC, c);
+        dfs(node->left, r + 1, c - 1, cache, minC, maxC);
+        dfs(node->right, r + 1, c + 1, cache, minC, maxC);
+    }
+};
+ 
+
+// Solution 2 
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
