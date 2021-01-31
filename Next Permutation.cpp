4efd974c763a9b15e38abf0e6de5da2d5009e77e
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-01-31 14:13:04 -0500

    Create Next Permutation.cpp

---
+++ Next Permutation.cpp
@@ -0,0 +1,61 @@
+/*
+Next Permutation
+
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order).
+The replacement must be in place and use only constant extra memory.
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+
+Example 2:
+
+Input: nums = [3,2,1]
+Output: [1,2,3]
+
+Example 3:
+
+Input: nums = [1,1,5]
+Output: [1,5,1]
+
+Example 4:
+
+Input: nums = [1]
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 100
+*/
+
+// Solution 1
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(begin(nums), end(nums));
+    }
+};
+
+// Solution 2
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+       int i = nums.size() - 1, j = i;
+        while (i > 0 && nums[i-1] >= nums[i])
+            i--;
+        for (int ii = i; ii < j; ii++, j--)
+            swap(nums[ii], nums[j]);
+        if (i > 0) {
+            j = i--;
+            while (nums[j] <= nums[i])
+                j++;
+            swap(nums[i], nums[j]);
+        }
+    }
+};
