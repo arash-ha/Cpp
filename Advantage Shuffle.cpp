8ae661c8e529d7773a05fa0be7d1c2aec3b30a97
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-03-24 19:23:14 -0400

    Create Advantage Shuffle.cpp

---
+++ Advantage Shuffle.cpp
@@ -0,0 +1,37 @@
+/*
+Advantage Shuffle
+
+
+Given two arrays A and B of equal size, the advantage of A with respect to B is the number of indices i for which A[i] > B[i].
+Return any permutation of A that maximizes its advantage with respect to B.
+
+Example 1:
+
+Input: A = [2,7,11,15], B = [1,10,4,11]
+Output: [2,11,7,15]
+
+Example 2:
+
+Input: A = [12,24,8,32], B = [13,25,32,11]
+Output: [24,32,8,12]
+ 
+Note:
+
+1 <= A.length = B.length <= 10000
+0 <= A[i] <= 10^9
+0 <= B[i] <= 10^9
+*/
+
+class Solution {
+public:
+    vector<int> advantageCount(vector<int>& A, vector<int>& B) {
+        vector<int> res;
+        sort(A.begin(), A.end());
+        for(int i = 0; i < B.size(); i++){
+            auto it = A.back() > B[i] ? upper_bound(A.begin(), A.end(), B[i]) : A.begin();
+            res.emplace_back(*it);
+            A.erase(it);
+        }
+        return res;
+    }
+};
