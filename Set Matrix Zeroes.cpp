69f72c05e392691bb5167167e8041806dc3a03d7
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-08-13 12:34:01 -0400

    Update Set Matrix Zeroes.cpp

--- Set Matrix Zeroes.cpp
+++ Set Matrix Zeroes.cpp
@@ -1,40 +1,34 @@
 /*
 Set Matrix Zeroes
 
-Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in-place.
-
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's, and return the matrix.
+You must do it in place.
+ 
 Example 1:
 
-Input: 
-[
-  [1,1,1],
-  [1,0,1],
-  [1,1,1]
-]
-Output: 
-[
-  [1,0,1],
-  [0,0,0],
-  [1,0,1]
-]
+Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[1,0,1],[0,0,0],[1,0,1]]
 
 Example 2:
 
-Input: 
-[
-  [0,1,2,0],
-  [3,4,5,2],
-  [1,3,1,5]
-]
-Output: 
-[
-  [0,0,0,0],
-  [0,4,5,0],
-  [0,3,1,0]
-]
+Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+Constraints:
+
+m == matrix.length
+n == matrix[0].length
+1 <= m, n <= 200
+-2^31 <= matrix[i][j] <= 2^31 - 1
+
+Follow up:
+
+A straightforward solution using O(mn) space is probably a bad idea.
+A simple improvement uses O(m + n) space, but still not the best solution.
+Could you devise a constant space solution?
 */
 
-
+// Solution 1
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
@@ -82,6 +76,30 @@ public:
             for (int j = 0; j < matrix[0].size(); j++) {
                 matrix[0][j] = 0;
             }
+        }
+    }
+};
+
+//Solution 2
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        bool col0 = false;
+        int m = matrix.size(), n = matrix[0].size();
+        for(int i = 0; i < m; i++){
+            if(matrix[i][0] == 0)
+                col0 = true;
+            for(int j = 1; j < n; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+
+        for(int i = m - 1; i >= 0; i--){
+            for(int j = n - 1; j >= 1; j--)
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if(col0) matrix[i][0] = 0;
         }
     }
 };
