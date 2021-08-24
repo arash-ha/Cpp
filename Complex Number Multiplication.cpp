e06322cae5625f97194a8432769d4870ad15a408
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-08-24 13:52:16 -0400

    Create Complex Number Multiplication.cpp

---
+++ Complex Number Multiplication.cpp
@@ -0,0 +1,42 @@
+/*
+Complex Number Multiplication
+
+A complex number can be represented as a string on the form "real+imaginaryi" where:
+real is the real part and is an integer in the range [-100, 100].
+imaginary is the imaginary part and is an integer in the range [-100, 100].
+i2 == -1.
+Given two complex numbers num1 and num2 as strings, return a string of the complex number that represents their multiplications.
+
+Example 1:
+
+Input: num1 = "1+1i", num2 = "1+1i"
+Output: "0+2i"
+Explanation: (1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i, and you need convert it to the form of 0+2i.
+
+Example 2:
+
+Input: num1 = "1+-1i", num2 = "1+-1i"
+Output: "0+-2i"
+Explanation: (1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i, and you need convert it to the form of 0+-2i.
+ 
+Constraints:
+
+num1 and num2 are valid complex numbers.
+*/
+
+class Solution {
+public:
+    string complexNumberMultiply(string num1, string num2) {
+        pair<int, int> a = getParam(num1), b = getParam(num2);
+        int real_a = a.first, imag_a = a.second;
+        int real_b = b.first, imag_b = b.second;
+        return to_string(real_a * real_b - imag_a * imag_b) + '+' + to_string(real_a * imag_b  + real_b * imag_a) + 'i' ;
+    }
+    pair<int, int> getParam(string num){
+        int p = num.find('+');
+        double real = stoi(num.substr(0, p));
+        double imaginary = stoi(num.substr(p + 1, num.size() - p - 2));
+        pair<int, int> res(real, imaginary);
+        return res;
+    }
+};
