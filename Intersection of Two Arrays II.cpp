a3be29e9932d94b6c8d47804bc2f6052169ca1b3
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-09-17 08:21:27 -0400

    Update Intersection of Two Arrays II.cpp

--- Intersection of Two Arrays II.cpp
+++ Intersection of Two Arrays II.cpp
@@ -1,19 +1,23 @@
 /*
 Intersection of Two Arrays II
+
 Given two arrays, write a function to compute their intersection.
 
 Example 1:
 
 Input: nums1 = [1,2,2,1], nums2 = [2,2]
 Output: [2,2]
+
 Example 2:
 
 Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 Output: [4,9]
+
 Note:
 
 Each element in the result should appear as many times as it shows in both arrays.
 The result can be in any order.
+
 Follow up:
 
 What if the given array is already sorted? How would you optimize your algorithm?
@@ -24,14 +28,13 @@ What if elements of nums2 are stored on disk, and the memory is limited such tha
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> map;
-        vector<int> vec{};
-        for(int i = 0; i < nums1.size(); i++) map[nums1[i]]++;
-        for(int j = 0; j < nums2.size(); j++){
-            if (map [nums2[j]]-- > 0) {
-                vec.push_back(nums2[j]);
-            }
-        }
-        return vec;
+        unordered_map<int, int> ump;
+        vector<int> res;
+        for(auto i : nums1)
+            ump[i]++;
+        for(auto j : nums2)
+            if (ump[j]-- > 0)
+                res.emplace_back(j);
+        return res;
     }
 };
