cc31e6c1abca808e382ae3532ae3fb13f98a8b42
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-10-21 09:11:35 -0400

    Update Insert Delete GetRandom O(1).cpp

--- Insert Delete GetRandom O(1).cpp
+++ Insert Delete GetRandom O(1).cpp
@@ -1,40 +1,40 @@
 /*
 Insert Delete GetRandom O(1)
-Design a data structure that supports all following operations in average O(1) time.
 
- 
+Implement the RandomizedSet class:
 
-insert(val): Inserts an item val to the set if not already present.
-remove(val): Removes an item val from the set if present.
-getRandom: Returns a random element from current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
- 
+RandomizedSet() Initializes the RandomizedSet object.
+bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
+You must implement the functions of the class such that each function works in average O(1) time complexity.
 
-Example:
+Example 1:
 
-// Init an empty set.
-RandomizedSet randomSet = new RandomizedSet();
+Input
+["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+[[], [1], [2], [2], [], [1], [2], []]
+Output
+[null, true, false, true, 2, true, false, 2]
 
-// Inserts 1 to the set. Returns true as 1 was inserted successfully.
-randomSet.insert(1);
+Explanation
+RandomizedSet randomizedSet = new RandomizedSet();
+randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
+randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
+randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+randomizedSet.insert(2); // 2 was already in the set, so return false.
+randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
 
-// Returns false as 2 does not exist in the set.
-randomSet.remove(2);
+Constraints:
 
-// Inserts 2 to the set, returns true. Set now contains [1,2].
-randomSet.insert(2);
-
-// getRandom should return either 1 or 2 randomly.
-randomSet.getRandom();
-
-// Removes 1 from the set, returns true. Set now contains [2].
-randomSet.remove(1);
-
-// 2 was already in the set, so return false.
-randomSet.insert(2);
-
-// Since 2 is the only number in the set, getRandom always return 2.
-randomSet.getRandom();
+-2^31 <= val <= 2^31 - 1
+At most 2 * 10^5 calls will be made to insert, remove, and getRandom.
+There will be at least one element in the data structure when getRandom is called.
 */
+
+// Solution I
 
 class RandomizedSet {
     
@@ -71,6 +71,48 @@ public:
     int getRandom() {
         return vec[rand() % vec.size()];
     }
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet* obj = new RandomizedSet();
+ * bool param_1 = obj->insert(val);
+ * bool param_2 = obj->remove(val);
+ * int param_3 = obj->getRandom();
+ */
+ 
+ // Solution II
+ 
+ class RandomizedSet {
+public:
+    RandomizedSet() {
+        
+    }
+    
+    bool insert(int val) {
+        if(ump.find(val) != ump.end())
+            return false;
+        nums.emplace_back(val);
+        ump[val] = nums.size() - 1;
+        return true;
+    }
+    
+    bool remove(int val) {
+        if(ump.find(val) == ump.end())
+            return false;
+        int last = nums.back();
+        ump[last] = ump[val];
+        nums[ump[val]] = last;
+        nums.pop_back();
+        ump.erase(val);
+        return true;
+    }
+    
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+    vector<int> nums;
+    unordered_map<int, int> ump;
 };
 
 /**
