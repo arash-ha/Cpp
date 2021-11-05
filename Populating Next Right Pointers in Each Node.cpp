6449407dd331f4b101a6270d59a0dd457478e365
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-11-05 08:47:17 -0400

    Update Populating Next Right Pointers in Each Node.cpp

--- Populating Next Right Pointers in Each Node.cpp
+++ Populating Next Right Pointers in Each Node.cpp
@@ -9,20 +9,14 @@ struct Node {
   Node *next;
 }
 Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
-
 Initially, all next pointers are set to NULL.
-
- 
 
 Follow up:
 
 You may only use constant extra space.
-Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
- 
+Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem. 
 
 Example 1:
-
-
 
 Input: root = [1,2,3,4,5,6,7]
 Output: [1,#,2,3,#,4,5,6,7,#]
@@ -46,7 +40,7 @@ public:
 };
 */
 
-// Solution 1
+// Solution I
 
 class Solution {
 public:
@@ -62,7 +56,7 @@ public:
     }
 };
 
-// Solution 2
+// Solution II
 
 class Solution {
 public:
@@ -82,6 +76,51 @@ public:
                     q.push(node->left);
                 if(node->right)
                     q.push(node->right);
+            }
+        }
+        return root;
+    }
+};
+
+// Solution III
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root)
+            return root;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            int s = q.size();
+            Node* r = nullptr;
+            while(s--){
+                Node* cur = q.front();
+                cur->next = r;
+                r = cur;
+                q.pop();
+                if(cur->right)
+                    q.push(cur->right);
+                if(cur->left)
+                    q.push(cur->left);
             }
         }
         return root;
