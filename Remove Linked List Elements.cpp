79aca34bdbb36686d9c66e13b0838a4214aa87e4
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-11-12 09:02:40 -0500

    Update Remove Linked List Elements.cpp

--- Remove Linked List Elements.cpp
+++ Remove Linked List Elements.cpp
@@ -7,6 +7,9 @@ Example:
 Input:  1->2->6->3->4->5->6, val = 6
 Output: 1->2->3->4->5
 */
+
+// Solution I
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -24,6 +27,37 @@ public:
         if(head -> val == val) return removeElements(head -> next, val);
         head -> next = removeElements(head -> next, val);
         return head;
+    }
+};
+
+// Solution II
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(!head)
+            return head;
+        while(head && head->val == val)
+            head = head->next;
+        ListNode *p1 = head, *p2 = nullptr;
+        while(p1){
+            if(p1->val == val)
+                p2->next = p1->next;
+            else
+                p2 = p1;
+            p1 = p1->next;
+        }
+        return head;  
     }
 };
 
