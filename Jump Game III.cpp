3de717ea3c56e65a385cf2edfdb5a4f49fd2afd8
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2021-12-09 14:29:00 -0500

    Update Jump Game III.cpp

--- Jump Game III.cpp
+++ Jump Game III.cpp
@@ -2,10 +2,7 @@
 Jump Game III
 
 Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
-
 Notice that you can not jump outside of the array at any time.
-
- 
 
 Example 1:
 
@@ -15,6 +12,7 @@ Explanation:
 All possible ways to reach at index 3 with value 0 are: 
 index 5 -> index 4 -> index 1 -> index 3 
 index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+
 Example 2:
 
 Input: arr = [4,2,3,0,3,1,2], start = 0
@@ -22,13 +20,13 @@ Output: true
 Explanation: 
 One possible way to reach at index 3 with value 0 is: 
 index 0 -> index 4 -> index 1 -> index 3
+
 Example 3:
 
 Input: arr = [3,0,2,1,2], start = 2
 Output: false
 Explanation: There is no way to reach at index 1 with value 0.
  
-
 Constraints:
 
 1 <= arr.length <= 5 * 10^4
@@ -36,6 +34,7 @@ Constraints:
 0 <= start < arr.length
 */
 
+// Solution I
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
@@ -49,5 +48,21 @@ public:
             return true;
         visited[index] = true;
         return helper(arr, index + arr[index], visited) || helper(arr, index - arr[index], visited);
+    }
+};
+
+// Solution II
+
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        if(arr[start]== -1)
+            return false;
+        if(!arr[start])
+            return true;     
+        int leftside= start - arr[start];
+        int rightside = start + arr[start];
+        arr[start] = -1;
+        return rightside = (rightside < arr.size() && canReach(arr, rightside) ) || (leftside >= 0 && canReach(arr, leftside));
     }
 };
