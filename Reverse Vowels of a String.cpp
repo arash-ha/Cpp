366ae9a3f5d8cca9004d7ecdeb809f9a627f797c
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2022-03-11 19:09:41 -0500

    Create Reverse Vowels of a String.cpp

---
+++ Reverse Vowels of a String.cpp
@@ -0,0 +1,37 @@
+/*
+Reverse Vowels of a String
+
+Write a function that takes a string as input and reverse only the vowels of a string.
+
+Example 1:
+
+Input: "hello"
+Output: "holle"
+
+Example 2:
+
+Input: "leetcode"
+Output: "leotcede"
+Note:
+The vowels does not include the letter "y".
+*/
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        vector<char> vec{};
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+                vec.push_back(s[i]);
+            }
+        }
+        int k = vec.size() - 1;
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U'){
+                swap(s[i], vec[k]);
+                k--;
+            }
+        }
+        return s;
+    }
+};
