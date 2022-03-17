15cd68a4ce197117ab6ad7ddd84ae877b71c40e7
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2022-03-17 09:38:47 -0400

    Update Score of Parentheses.cpp

--- Score of Parentheses.cpp
+++ Score of Parentheses.cpp
@@ -1,9 +1,7 @@
 /*
 Score of Parentheses
 
-
 Given a balanced parentheses string S, compute the score of the string based on the following rule:
-
 () has score 1
 AB has score A + B, where A and B are balanced parentheses strings.
 (A) has score 2 * A, where A is a balanced parentheses string.
@@ -28,13 +26,12 @@ Example 4:
 Input: "(()(()))"
 Output: 6
  
-
 Note:
 
 S is a balanced parentheses string, containing only ( and ).
 2 <= S.length <= 50
 */
-
+// Solution I
 class Solution {
 public:
     int scoreOfParentheses(string S) {
@@ -49,5 +46,26 @@ public:
                 a--;
         }
         return res;
+    }
+};
+
+// Solution II
+
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack < int > st;
+        st.push(0);
+        for(char c: s){
+            if(c == ')'){
+                int top = st.top();
+                st.pop();
+                int val = (top > 0) ? top * 2 : 1;                
+                st.top() += val;
+            }
+            else
+                st.push(0);
+        }
+        return st.top();
     }
 };
