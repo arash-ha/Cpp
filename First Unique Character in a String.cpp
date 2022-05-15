33ca1f5f97e3426e9fe0822c9fe415df03c3c232
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2022-05-15 16:43:07 -0400

    Update First Unique Character in a String.cpp

--- First Unique Character in a String.cpp
+++ First Unique Character in a String.cpp
@@ -1,17 +1,30 @@
 /*
 First Unique Character in a String
-Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
 
-Examples:
+Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 
-s = "leetcode"
-return 0.
+Example 1:
 
-s = "loveleetcode",
-return 2.
-Note: You may assume the string contain only lowercase letters.
+Input: s = "leetcode"
+Output: 0
+
+Example 2:
+
+Input: s = "loveleetcode"
+Output: 2
+
+Example 3:
+
+Input: s = "aabb"
+Output: -1
+
+Constraints:
+
+1 <= s.length <= 10^5
+s consists of only lowercase English letters.
 */
 
+// Solution I
 class Solution {
 public:
     int firstUniqChar(string s) {
@@ -25,4 +38,22 @@ public:
         }
         return -1;
     }
+};
+
+// Solution II
+class Solution{
+public:	
+	bool isUniqueChars(string str){
+		
+		if(str.length() > 256) return false;
+		
+		bool char_set[256] = {false};
+		
+		for(int i = 0; i < str.length(); i++){
+			int val = str[i];
+			if(char_set[val]) return false;
+			char_set[val] = true;
+		}
+		return true;
+	}
 };
