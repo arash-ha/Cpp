af1407942bd7f2165336d089849af68d29ebba12
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2022-05-16 13:13:19 -0400

    Update Perfect Squares.cpp

--- Perfect Squares.cpp
+++ Perfect Squares.cpp
@@ -22,6 +22,7 @@ Constraints:
 1 <= n <= 10^4
 */
 
+// Solution I
 class Solution {
 public:
     int numSquares(int n) {
@@ -32,5 +33,25 @@ public:
                 dp[i] = min(dp[i - k * k] + 1, dp[i]);
         }
         return dp[n];
+    }
+};
+
+// SOlution II
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num < 0) return -1;
+        else if (num == 0) return 0;
+        long mid, low = 0, high = num;
+        while(low <= high){
+        mid = (low + high)/2;
+        if (mid*mid == num) return true;
+        else{
+            if((mid * mid < num && (mid + 1)*(mid + 1) > num)) return false;
+            else if (mid * mid < num)  low = mid + 1;   
+            else high = mid - 1;
+        }
+        }
+        return mid;
     }
 };
