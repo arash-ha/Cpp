7e0b64668c116f3a74c92d84e86cdff5d0343c39
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2022-05-27 09:12:24 -0400

    Update Number of Steps to Reduce a Number to Zero.cpp

--- Number of Steps to Reduce a Number to Zero.cpp
+++ Number of Steps to Reduce a Number to Zero.cpp
@@ -41,14 +41,11 @@ public:
     int numberOfSteps (int num) {
         int step = 0;
         while(num != 0){
-            if(num % 2 == 0){
+            if(num % 2 == 0)
                 num /= 2;
-                step++;
-            }
-            else{
+            else
                 num--;
-                step++;
-            }
+            step++;
         }
         return step;
     }
