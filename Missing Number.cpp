7b1b320978e69934a15862ce8f94550a18a28e9e
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2022-05-27 20:28:47 -0400

    Update Missing Number.cpp

--- Missing Number.cpp
+++ Missing Number.cpp
@@ -36,6 +36,7 @@ n == nums.length
 All the numbers of nums are unique.
 */
 
+// Solution I
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
@@ -45,6 +46,21 @@ public:
         for(int i = 0; i < nums.size(); i++){
             if(!ump.count(i))
                 return i;
+        }
+        return nums.size();
+    }
+};
+
+// Solution II
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if(nums[0] != 0)
+            return 0;
+        for(int i = 0; i < nums.size() - 1; i++){
+            if(nums[i + 1] != nums[i] + 1)
+                return i + 1;
         }
         return nums.size();
     }
