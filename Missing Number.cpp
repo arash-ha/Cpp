ed38db70ed0801949f1303035bbff2e43a80f6a3
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2022-05-28 07:55:10 -0400

    Update Missing Number.cpp

--- Missing Number.cpp
+++ Missing Number.cpp
@@ -59,7 +59,7 @@ public:
         if(nums[0] != 0)
             return 0;
         for(int i = 0; i < nums.size() - 1; i++){
-            if(nums[i + 1] != nums[i] + 1)
+            if(nums[i + 1] != ++nums[i])
                 return i + 1;
         }
         return nums.size();
