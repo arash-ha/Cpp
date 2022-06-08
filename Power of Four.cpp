1b5ea500d6940e5f2f7c2e7baa425b4e1d41a7ee
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2022-06-08 08:51:14 -0400

    Create Power of Four.cpp

---
+++ Power of Four.cpp
@@ -0,0 +1,30 @@
+/*
+Power of Four
+
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+
+Example 2:
+
+Input: 5
+Output: false
+
+Constraints:
+
+-2^31 <= n <= 2^31 - 1
+*/
+
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if(num == 0) return false;
+        if(num == 1) return true;
+        if(num % 4 == 0) return isPowerOfFour(num/4);
+        return false;
+    }
+};
