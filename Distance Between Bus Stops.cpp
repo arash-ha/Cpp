cab5d5ddadbd366c16d81de5d1b64be67215ec87
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2022-11-20 10:58:16 -0500

    Update Distance Between Bus Stops.cpp

--- Distance Between Bus Stops.cpp
+++ Distance Between Bus Stops.cpp
@@ -37,8 +37,8 @@ public:
         int sum1 = 0, sum2 = 0;
         if(start > destination) 
             swap(start, destination);
-        for(auto i = 0; i < distance.size(); ++i){
-            if (i >= start && i < destination) 
+        for(auto i = 0; i < distance.size(); i++){
+            if(i >= start && i < destination) 
                 sum1 += distance[i];
             else
                 sum2 += distance[i];
