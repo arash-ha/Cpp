3b001966e47c3b81c5a8fc53eb98c90aa1ef8671
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2023-04-09 11:18:04 -0400

    Update Clone Graph.cpp

--- Clone Graph.cpp
+++ Clone Graph.cpp
@@ -82,7 +82,7 @@ public:
     }
 };
 */
-
+// Solution I
 class Solution {
 public:
     unordered_map<int, Node*> ump;
@@ -95,5 +95,33 @@ public:
             newNode->neighbors.emplace_back(cloneGraph(n));
         }
         return newNode;
+    }
+};
+
+// Solution II
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node*, Node*> mp;
+        if(node == NULL)
+            return NULL;
+        if(node->neighbors.size() == 0){
+            Node* clone= new Node(node->val);
+            return clone; 
+        }
+        return dfs(node, mp);
+    }
+    Node* dfs(Node* cur,unordered_map<Node*, Node*>& mp){
+        vector<Node*> neighbour;
+        Node* clone=new Node(cur->val);
+        mp[cur]=clone;
+            for(auto it:cur->neighbors){
+                if(mp.find(it)!=mp.end())
+                    neighbour.push_back(mp[it]);
+                else
+                    neighbour.push_back(dfs(it,mp));
+            }
+            clone->neighbors = neighbour;
+            return clone;
     }
 };
