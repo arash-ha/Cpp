51115d6d14d609dcff4dfa731ee98bda318a446d
Arash Ahangarnejad <54961176+arash-ha@users.noreply.github.com>
2023-08-17 07:53:08 -0700

    Update Sliding Window Maximum.cpp

--- Sliding Window Maximum.cpp
+++ Sliding Window Maximum.cpp
@@ -1,10 +1,8 @@
 /*
 Sliding Window Maximum
+
 You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
-
 Return the max sliding window.
-
- 
 
 Example 1:
 
@@ -19,31 +17,20 @@ Window position                Max
  1  3  -1 [-3  5  3] 6  7       5
  1  3  -1  -3 [5  3  6] 7       6
  1  3  -1  -3  5 [3  6  7]      7
+
 Example 2:
 
 Input: nums = [1], k = 1
 Output: [1]
-Example 3:
-
-Input: nums = [1,-1], k = 1
-Output: [1,-1]
-Example 4:
-
-Input: nums = [9,11], k = 2
-Output: [11]
-Example 5:
-
-Input: nums = [4,-2], k = 2
-Output: [4]
  
-
 Constraints:
 
-1 <= nums.length <= 105
--104 <= nums[i] <= 104
+1 <= nums.length <= 10^5
+-10^4 <= nums[i] <= 10^4
 1 <= k <= nums.length
 */
 
+// Solution I
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
@@ -60,5 +47,28 @@ public:
             }
         }
         return res;
+    }
+};
+
+// Solution II
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        int left = 0, right = 0;
+        deque<int> q;
+        while (right < nums.size()) {
+            while (!q.empty() && nums[right] > nums[q.back()])
+                q.pop_back();
+            q.push_back(right);
+            if (left > q.front())
+                q.pop_front();
+            if (right + 1 >= k) {
+                res.push_back(nums[q.front()]);
+                left++;
+            }
+            right++;
+        }
+        return res;   
     }
 };
